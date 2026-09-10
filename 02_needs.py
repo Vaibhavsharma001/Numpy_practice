80f6596a5604de22c51c23dee17dfc18db0d9047
vaibhavsharma <vaibhav53091@gmail.com>
2026-09-10 19:25:24 +0530

    Add need of numpy

---
+++ 02_needs.py
@@ -0,0 +1,23 @@
+# Why Do We Need NumPy?
+# English
+
+# Python Lists are good for storing data, but they become slow when performing mathematical operations on large datasets. I
+
+# NumPy solves this problem by providing:
+
+# Faster calculations
+# Less memory usage
+# Vectorized operations
+# Powerful mathematical functions
+# Multi-dimensional arrays
+# Hindi
+
+# Python List छोटे डेटा के लिए अच्छी होती है लेकिन बड़े डेटा पर Mathematical Opertions करने में धीमी होती है।
+
+# NumPy हमें देता है:
+
+# Fast Calculation
+# कम Memory Usage
+# Large Dataset Support
+# Scientific Functions
+# High Performance
