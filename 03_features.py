eef8b999c5d218735b0972e51cd834ede629addb
vaibhavsharma <vaibhav53091@gmail.com>
2026-09-10 19:26:29 +0530

    Add features of numpy

---
+++ 03_features.py
@@ -0,0 +1,20 @@
+                                 # Features of NumPy
+                                 
+# English
+# Fast Execution
+# Multi-dimensional Arrays
+# Mathematical Functions
+# Memory Efficient
+# Broadcasting
+# Linear Algebra
+# Random Number Generation
+
+
+# Hindi
+# तेज़ गति से काम करता है
+# कई Dimension वाले Arrays
+# बहुत सारे Math Functions
+# कम Memory उपयोग करता है
+# Different Shape Arrays 47 Operation
+# Matrix Operations
+# Random Data Generate करता है
